b67929af144da7f81ade7bbd835c8f884dcd4ab3
sewaca <53119694+sewaca@users.noreply.github.com>
2020-09-09 22:15:30 +0500

    Add files via upload

--- webdev/js/svg-hovers.js
+++ webdev/js/svg-hovers.js
@@ -1,1 +1,32 @@
-var paths=document.getElementsByClassName('facebookPath'),i=paths.length;while(i--){paths[i].parentElement.parentElement.parentElement.onmouseover=function(){this.children[0].children[0].children[0].setAttribute('fill','#ffffff')}paths[i].parentElement.parentElement.parentElement.onmouseleave=function(){this.children[0].children[0].children[0].setAttribute('fill','#00897b')}}var paths=document.getElementsByClassName('twitterPath'),i=paths.length;while(i--){paths[i].parentElement.parentElement.onmouseover=function(){this.children[0].children[0].setAttribute('fill','#ffffff')}paths[i].parentElement.parentElement.onmouseleave=function(){this.children[0].children[0].setAttribute('fill','#00897b')}}var paths=document.getElementsByClassName('wechatPath'),i=paths.length;while(i--){paths[i].parentElement.parentElement.onmouseover=function(){this.children[0].children[0].setAttribute('fill','#ffffff')}paths[i].parentElement.parentElement.onmouseleave=function(){this.children[0].children[0].setAttribute('fill','#00897b')}}
+var paths = document.getElementsByClassName('facebookPath'),
+  i = paths.length;
+while (i--) {
+  paths[i].parentElement.parentElement.parentElement.onmouseover = function() {
+    this.children[0].children[0].children[0].setAttribute('fill', '#ffffff')
+  }
+  paths[i].parentElement.parentElement.parentElement.onmouseleave = function() {
+    this.children[0].children[0].children[0].setAttribute('fill', '#00897b')
+  }
+}
+
+var paths = document.getElementsByClassName('twitterPath'),
+  i = paths.length;
+while (i--) {
+  paths[i].parentElement.parentElement.onmouseover = function() {
+    this.children[0].children[0].setAttribute('fill', '#ffffff')
+  }
+  paths[i].parentElement.parentElement.onmouseleave = function() {
+    this.children[0].children[0].setAttribute('fill', '#00897b')
+  }
+}
+
+var paths = document.getElementsByClassName('wechatPath'),
+  i = paths.length;
+while (i--) {
+  paths[i].parentElement.parentElement.onmouseover = function() {
+    this.children[0].children[0].setAttribute('fill', '#ffffff')
+  }
+  paths[i].parentElement.parentElement.onmouseleave = function() {
+    this.children[0].children[0].setAttribute('fill', '#00897b')
+  }
+}
