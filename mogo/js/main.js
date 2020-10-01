062199d0f4e5cb39b42e4f3569ec82dde33e3f51
sewaca <sewaca7@gmail.com>
2020-10-01 16:09:29 +0500

    htacces & mogo

--- mogo/js/main.js
+++ mogo/js/main.js
@@ -3,11 +3,57 @@ document.querySelectorAll('ul.tabs li button').forEach((item, i) => {
     event.target.parentElement.classList.toggle('active');
   }
 });
-var image = document.getElementsByClassName('paralaxImage');
-var ints = new simpleParallax(image, {
-  orientation: 'down',
-  overflow: true,
-  transition: '0s',
-  delay: 0,
-  scale: 2.8
-});
+var clientsPar, firstScreenParallax;
+function setParallaxClients(){
+  var a = setInterval(()=>{
+    if(document.querySelectorAll('#aboutUs .lazyload').length + document.querySelectorAll('#ourteam .lazyload').length +   document.querySelectorAll('#gallery .lazyload').length == 0){
+      var imgrev = document.querySelectorAll('#whatPeopleSay > .paralaxImage');
+      clientsPar = new simpleParallax(imgrev, {
+        orientation: 'down',
+        transition: '0s',
+        delay: 0,
+        overflow: true,
+        scale: 2.4,
+      });
+
+      clearInterval(a);
+   }
+  }, 100);
+}
+function setFirstScreenParallax(){
+  let image = document.querySelector('#firstScreen > .paralaxImage');
+  firstScreenParallax = new simpleParallax(image, {
+    orientation: 'down',
+    overflow: true,
+    transition: '0s',
+    delay: 0,
+    scale: 2.8
+  });
+}
+window.addEventListener('load', ()=>{
+  setTimeout(()=>{
+    window.scroll(0,0);
+  }, 1);
+
+  setFirstScreenParallax();
+  setParallaxClients();
+})
+
+window.addEventListener('resize', ()=>{
+  clientsPar.destroy();
+  firstScreenParallax.destroy();
+  setTimeout(()=>{
+    setParallaxClients();
+    setFirstScreenParallax();
+  }, 1);
+})
+document.querySelector('#mainfooter input').oninput = ()=>{
+  if ( document.querySelector('#mainfooter input').value.length == 0 ) document.querySelector('#mainfooter input').classList.add('empty');
+  else document.querySelector('#mainfooter input').classList.remove('empty');
+}
+document.querySelector('#mainfooter input').onfocus = ()=>{
+  if(document.querySelector('#mainfooter input').value == 'Your Email...')document.querySelector('#mainfooter input').value = '';
+}
+document.querySelector('#mainfooter input').onblur = ()=>{
+  if(document.querySelector('#mainfooter input').value == '')document.querySelector('#mainfooter input').value = 'Your Email...';
+}
